b5658330ebcf324b434c8925477a4263c8791fe8
Thomas Rasmussen <RasThomas@gmail.com>
2012-06-01 16:22:00 +0200

    WARNING: Redid the project, I have also added the excel records to the database, so don't run it again.

--- MedArt/src/record/DBConnection.java
+++ MedArt/src/record/DBConnection.java
@@ -5,24 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	
 
-  public static Connection connect() {
 
-	    Connection con = null;
+	public static Connection connect() {
 
-	    try {
-	      Class.forName("com.mysql.jdbc.Driver").newInstance();
-	      con = DriverManager.getConnection("jdbc:mysql://129.177.48.168:3306/mydb",
-	        "nisse", "komputer");
+		Connection con = null;
 
-	      if(!con.isClosed())
-	        System.out.println("Successfully connected to MySQL server");
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection("jdbc:mysql://129.177.48.187:3306/mydb",
+					"nisse", "komputer");
 
-	    } catch(Exception e) {
-	      System.err.println("Exception: " + e.getMessage());
-	      e.printStackTrace();
-	    } 
-	return con;
-  }
+			if(!con.isClosed())
+				System.out.println("Successfully connected to MySQL server");
+
+		} catch(Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			e.printStackTrace();
+		} 
+		return con;
+	}
 }
